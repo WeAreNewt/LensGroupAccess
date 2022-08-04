af7c1bcb442402284e1ba9066e1c8c8793d0125d
JosepBove <josep@aave.com>
2022-08-04 18:16:00 +0100

    Test && fixes

--- src/LensGroupAccess.sol
+++ src/LensGroupAccess.sol
@@ -1,24 +1,24 @@
 // SPDX-License-Identifier: BSD-3-Clause
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.10;
 
 
 import "@lens/interfaces/ILensHub.sol";
 import "@lens/libraries/DataTypes.sol";
 import "@solmate/auth/Owned.sol";
 
-contract LensGroupAccess {
+contract LensGroupAccess is Owned {
 
     ILensHub lensHub;
     address[] public whitelist;
     uint256 public profileId;
 
-    error NotWhitelisted(address);
-    error AlreadyWhitelisted(address);
+    error NotWhitelisted();
+    error AlreadyWhitelisted();
     event WhitelistAdded(address);
     event WhitelistRemoved(address);
     event PostCreated(DataTypes.PostData);
 
-    constructor (address _lensHubAddress, _profileId) Owned(msg.sender) {
+    constructor (address _lensHubAddress, uint256 _profileId) Owned(msg.sender) {
         lensHub = ILensHub(_lensHubAddress);
         profileId = _profileId;
         whitelist.push(msg.sender);
@@ -32,9 +32,7 @@ contract LensGroupAccess {
         address referenceModule,
         bytes calldata referenceModuleInitData
     ) external {
-        boolean found = false;
-      
-        if(!found) revert NotWhitelisted(msg.sender);
+        if(!isWhitelisted(msg.sender)) revert NotWhitelisted();
 
         DataTypes.PostData memory data = DataTypes.PostData(
             profileId,
@@ -56,19 +54,13 @@ contract LensGroupAccess {
     }
 
     function addWhitelist(address _address) external onlyOwner {
-        if(isWhitelisted(_address)) revert AlreadyWhitelisted(_address);
-        unchecked {
-            for(uint256 i = 0; i < whitelist.length; i++) {
-                if(msg.sender == whitelist[i]) found = true;
-            }
-        }
-        if(!found) whitelist.push(_address);
-
+        if(isWhitelisted(_address)) revert AlreadyWhitelisted();
+        whitelist.push(_address);
         emit WhitelistAdded(_address);
     }
 
     function removeWhitelist(address _address) external onlyOwner {
-         if(!isWhitelisted(_address)) revert NotWhitelisted(_address);
+         if(!isWhitelisted(_address)) revert NotWhitelisted();
 
          unchecked {
             for(uint256 i = 0; i < whitelist.length; i++) {
@@ -82,12 +74,12 @@ contract LensGroupAccess {
         emit WhitelistRemoved(_address);
     }
 
-    function isWhitelisted(address _address) internal returns(bool whitelisted) {
+    function isWhitelisted(address _address) public view returns(bool) {
         unchecked {
             for(uint256 i = 0; i < whitelist.length; i++) {
-                if(msg.sender == whitelist[i]) whitelisted = true;
+                if(_address == whitelist[i]) return true;
             }
-            whitelisted = false;
+            return false;
         }
     }
 
--- test/LensGroupAccess.t.sol
+++ test/LensGroupAccess.t.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.10;
 
 import "forge-std/Test.sol";
 import "forge-std/Vm.sol";
@@ -10,20 +10,80 @@ import '../src/LensGroupAccess.sol';
 
 contract LensGroupAccessTest is Test {
     uint256 public constant PROFILE_ID = 1408;
-    LensGroupAccess private lensGroupAccess;
+    LensGroupAccess lensGroupAccess;
 
     ILensHub lensHub = ILensHub(0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d);
 
     address addressWithDAOProfile = 0x14306f86629E6bc885375a1f81611a4208316B2b;
     address nftOwner = 0x7020AFb73882c715415810Cfe12946bf1f999a9b;
+    address user = address(3);
 
-    function setUp(
+    function setUp() public {
         lensGroupAccess = new LensGroupAccess(0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d, PROFILE_ID);
         vm.prank(addressWithDAOProfile);
-        lensHub.setDispatcher(PROFILE_ID, address(postDAO));
-    ) public {}
+        lensHub.setDispatcher(PROFILE_ID, address(lensGroupAccess));
+    }
 
-    function testExample() public {
-        assertTrue(true);
+    function testAddWhitelist() public {
+        lensGroupAccess.addWhitelist(user);
+        bool whitelisted = lensGroupAccess.isWhitelisted(user);
+        assertEq(whitelisted, true);
+    }
+
+    function testRemoveWhitelist() public {
+        bool whitelisted;
+        lensGroupAccess.addWhitelist(user);
+        whitelisted = lensGroupAccess.isWhitelisted(user);
+        assertEq(whitelisted, true);
+    }
+
+    function testRemoveWhitelistIfNotInWhitelist() public {
+        vm.expectRevert(abi.encodeWithSignature("NotWhitelisted()"));
+        lensGroupAccess.removeWhitelist(user);
+    }
+
+    function testAddWhitelistIfAlreadyInWhitelist() public {
+        lensGroupAccess.addWhitelist(user);
+        vm.expectRevert(abi.encodeWithSignature("AlreadyWhitelisted()"));
+        lensGroupAccess.addWhitelist(user);
+    }
+
+
+    function testPostIfWhitelisted() public {
+        DataTypes.ProfileStruct memory profile =  lensHub.getProfile(PROFILE_ID);
+
+        lensGroupAccess.addWhitelist(user);
+        bool whitelisted = lensGroupAccess.isWhitelisted(user);
+        assertEq(whitelisted, true);
+
+        vm.prank(user);
+
+        lensGroupAccess.post(
+            'aave.com', 
+            0x23b9467334bEb345aAa6fd1545538F3d54436e96, // test module, everyone can collect
+            abi.encode(false), 
+            address(0), 
+            abi.encode('')
+        );
+
+        assertEq(lensHub.getContentURI(PROFILE_ID, profile.pubCount+1), 'aave.com');
+    }
+
+    function testPostIfNotWhitelisted() public {
+        vm.prank(user);
+        vm.expectRevert(abi.encodeWithSignature("NotWhitelisted()"));
+        lensGroupAccess.post(
+            'aave.com', 
+            0x23b9467334bEb345aAa6fd1545538F3d54436e96, // test module, everyone can collect
+            abi.encode(false), 
+            address(0), 
+            abi.encode('')
+        );
+    }
+
+    function testSetProfileId() public {
+        uint256 dummyProfileId = 10;
+        lensGroupAccess.setProfileId(dummyProfileId);
+        assertEq(dummyProfileId, lensGroupAccess.profileId());
     }
 }
